13f7512cbba6aef0023f43733822a375f067a7c8
joshmasterton <joshmasterton@tuta.io>
2024-12-11 08:54:01 +0000

    Update cron job

--- api/src/config/cron.config.ts
+++ api/src/config/cron.config.ts
@@ -1,6 +1,6 @@
 import { CronJob } from 'cron';
 import { botVote } from '../bot/vote.bot';
 
-export const botVoteJob = new CronJob('*/1 * * * * *', async () => {
+export const botVoteJob = new CronJob('*/10 * * * * *', async () => {
   await botVote();
 });
